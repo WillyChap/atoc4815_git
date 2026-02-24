d1389d4f98019566d5d5720891b64caf15ff356d
Chandni Rajesh <chandnir2@gmail.com>
2026-02-24 10:41:39 -0700

    Added my name to file

--- boulder_temperatures.py
+++ boulder_temperatures.py
@@ -10,7 +10,7 @@ Instructions:
 """
 
 # TODO: Add your name here
-STUDENT_NAME = ""  # Example: "Alice Johnson"
+STUDENT_NAME = "Chandni Rajesh"  # Example: "Alice Johnson"
 
 
 def load_temperatures(filepath):
