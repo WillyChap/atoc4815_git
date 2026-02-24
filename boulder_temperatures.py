cedbaef2a2cf603dac7b5480456dceab37d81476
mamu9533 <mamu9533@colorado.edu>
2026-02-24 10:41:41 -0700

    added my name to file

--- boulder_temperatures.py
+++ boulder_temperatures.py
@@ -10,7 +10,7 @@ Instructions:
 """
 
 # TODO: Add your name here
-STUDENT_NAME = ""  # Example: "Alice Johnson"
+STUDENT_NAME = "Maxim Muter"  # Example: "Alice Johnson"
 
 
 def load_temperatures(filepath):
