a78b3c93288e42e59c693238dc64dbf85d38a714
mariac314 <maria.carter@colorado.edu>
2026-03-03 10:47:14 -0700

    edit on main

--- collaborative_analysis.py
+++ collaborative_analysis.py
@@ -1,6 +1,6 @@
 """
 Collaborative Weather Analysis -- edited on conflict-test
-ATOC 4815/5815 - Git Collaboration Exercise -- edit again
+ATOC 4815/5815 - Git Collaboration Exercise -- edit again on main
 
 Instructions:
   - Partner A: Complete the functions in SECTION A (temperature analysis)
